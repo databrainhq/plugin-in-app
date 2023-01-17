00c71202756b0043141a19a4f60bd98e6f2718fd
Aditya Yaduvanshi <aditya@usedatabrain.com>
2023-01-18 00:23:38 +0530

    tested plugin in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import './App.css'
-import '@databrainhq/plugin/dist/index.css'
+import '@databrainhq/plugin/dist/style.css'
 import { Dashboard } from '@databrainhq/plugin';
 
 function App() {
-
   return (
     <div>
-      <Dashboard token="" />
+      <Dashboard token="71b0b72a-1938-4055-ac24-f746a62f5685" />
     </div>
   )
 }
-
+ 
 export default App
